6d812a6bc1c412ddbf266772fae923c69846b371
vladyslav Kozar <vladyslav.kozar@apriorit.com>
2024-11-23 12:07:48 +0000

    jenkins HW modified terraform files

---
+++ Jenkins/main.tf
@@ -0,0 +1,188 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.9.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "aab380c1-3231-48a0-9116-3ba414023e17"
+}
+
+# Resourse groupe create
+resource "azurerm_resource_group" "rg" {
+  name     = "jenkCourseRG"
+  location = "West Europe"
+}
+
+# Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "course-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "course-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# NSG
+resource "azurerm_network_security_group" "nsg" {
+  name                = "course-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  
+# SSH 
+  security_rule {
+    name                       = "ssh"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "95.46.143.103/32"
+    destination_address_prefix = "*"
+  }
+
+# http 
+  security_rule {
+    name                       = "http"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "95.46.143.103/32"
+    destination_address_prefix = "*"
+  }
+  
+# http 2
+  security_rule {
+    name                       = "http2"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "95.46.143.103/32"
+    destination_address_prefix = "*"
+  }
+  
+}
+
+# Connect subnet and NSG
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# IP 
+resource "azurerm_public_ip" "pip1" {
+  name                = "IPForLB-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_public_ip" "pip2" {
+  name                = "IPForLB-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+# NICs
+resource "azurerm_network_interface" "nic1" {
+  name                = "course-nic1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+	public_ip_address_id          = azurerm_public_ip.pip1.id
+  }
+}
+
+resource "azurerm_network_interface" "nic2" {
+  name                = "course-nic2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+	public_ip_address_id          = azurerm_public_ip.pip2.id
+  }
+}
+
+# Create VMs
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                  = "terrafrom-lesson-vm1"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  size                  = "Standard_B1s"
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.nic1.id]
+  admin_password        = "Testpassword1"
+  disable_password_authentication = false
+  
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  tags = {
+    environment = "testing"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm2" {
+  name                  = "terrafrom-lesson-vm2"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  size                  = "Standard_B1s"
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.nic2.id]
+  admin_password        = "Testpassword1"
+  disable_password_authentication = false
+  
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  tags = {
+    environment = "testing"
+  }
+}
+
+# Output the Public IP of the Load Balancer
+output "azurerm_public_ip" {
+  value = [azurerm_public_ip.pip1, azurerm_public_ip.pip2]
+}
